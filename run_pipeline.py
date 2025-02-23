4e23f6eca19351e6cc85c67c1045103569e3bfa2
Kamalesh-Gandhi <gkamalesh1214@gmail.com>
2025-02-24 00:45:11 +0530

    Delete run_pipeline.py

--- run_pipeline.py
+++
@@ -1,5 +0,0 @@
-from Pipelines.clickstream_pipeline import clickstream_pipeline
-
-if __name__ == '__main__':
-    #Run the Pipeline
-    clickstream_pipeline("data\Train_data.csv", "data\Test_data.csv")
